a0b964ce4fe97eee66a9f590cd909b986fdab7c8
GoJek <gojek@Sumits-MacBook-Pro.local>
2017-05-23 12:56:30 +0530

    Div Function added with testing

--- src/calculator.js
+++ src/calculator.js
@@ -1,13 +1,19 @@
 function sum(a, b) {
-  return a + b;
+  return a+b;
 }
 
+function div(a, b) {
+  if(b===0)
+  	return "Not possible";
+  return a/b;
+}
 module.exports = {
-  sum: sum 
+  sum: sum, 
+  div: div
 }
 
 
-/*exports.sum = function (a, b) {
-  return a + b;
-}*/
+// exports.sum = function (a, b) {
+//   return a + b;
+// }
 // exports.sum = (a, b) => a + b;
--- test/calculator-test.js
+++ test/calculator-test.js
@@ -1,9 +1,20 @@
 var assert = require('assert');
 var Calculator = require('../src/calculator');
 describe('Calculator', function() {
+
   describe('sum', function() {
     it('should return 2 when inputs are 1,1', function() {
       assert.equal(2, Calculator.sum(1,1));
     });
+  });
+
+  describe('div', function() {
+    it('should return 0 when inputs are 0,1', function() {
+      assert.equal(0, Calculator.div(0,1));
+  	});
+    it('should return "Not possible" when inputs are 1,0', function() {
+      assert.equal("Not possible", Calculator.div(1,0));
+  	});
+
   });
 });
